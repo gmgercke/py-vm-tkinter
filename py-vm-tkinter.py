13189d33dc3de4017d56af21b60ca805cc9287fb
gmgercke <82462147+gmgercke@users.noreply.github.com>
2024-04-04 14:05:22 +0200

    Documentation, comments

--- py-vm-tkinter.py
+++ py-vm-tkinter.py
@@ -1,10 +1,10 @@
 import os
 import random
 import tkinter as tk
+from tkinter import ttk
 import tkinter.font
 
 # Definition des tkinter Fensters und Überschrift, sowie verschiedenen Fonts
-
 window = tk.Tk()
 window.title("Getränkeautomat")
 window.config(border=25)
@@ -13,9 +13,8 @@ mainFont = tkinter.font.Font(family="Helvetica", size=12)
 greeting = tk.Label(text="Willkommen bei Trink-O-Mat!", font=greetingFont)
 greeting.grid()
 
+
 # Zuerst definiere ich das Objekt "Drink", das einen Namen und einen Preis bekommt
-
-
 class Drink:
     def __init__(self, name, price):
         self.name = name
@@ -29,7 +28,6 @@ d4 = Drink("Wasser", 0.75)
 d5 = Drink("Gummibärchen", 0.00)
 
 # Hier packe ich die Namen und Preise der Objekte in Listen, um sie später einfacher durchsuchen zu können
-
 drinks = [d1.name, d2.name, d3.name, d4.name, d5.name]
 prices = [d1.price, d2.price, d3.price, d4.price, d5.price]
 
@@ -46,52 +44,57 @@ drinklist.grid()
 varChoice = tk.StringVar()
 varNumber = tk.StringVar()
 
+
 # Funktion: Vorschautext in Eingabefeld verschwindet beim Reinklicken
-
-
 def on_focus_in(entry):
     if entry.cget('state') == 'disabled':
         entry.configure(state='normal')
         entry.delete(0, 'end')
 
+
 # Funktion: Vorschautext in Eingabefeld kehrt zurück beim Rausklicken
-
-
 def on_focus_out(entry, placeholder):
     if entry.get() == "":
         entry.insert(0, placeholder)
         entry.configure(state='disabled')
 
-# Funktion: Button: Nutzer gibt ein, welches Getränk er möchte.
-# Wenn das Getränk in der "drinks" Liste existiert geht es unten weiter.
-# Wenn das Getränk in der Liste nicht existiert, ended das Programm mit einer Fehlermeldung.
 
-
+# Labels werden als existent vordefiniert, um sie später ausserhalb der Schleife manipulieren zu können
 auswahl = tk.Label(window)
 error = tk.Label(window)
 bezahl = tk.Label(window)
 surprise = tk.Label(window)
+gsurprise = tk.Label(window)
 
-
+# Funktion: Button: Nutzer gibt ein, welches Getränk und wieviel davon er möchte, klickt dann Bestellen
 def submit():
-    choiceSet = varChoice.get()
+    # Die Variablen für Auswahl und Menge werden als "leer" vordefiniert
+    choiceSet = choiceEntry.get()
     varChoice.set("")
-    numberSet = (varNumber.get())
+    numberSet = varNumber.get()
     varNumber.set("")
 
+    # Die Variablen werden als global vordefiniert, um sie ausserhalb der Schleife manipulieren zu können
     global auswahl
     global error
     global bezahl
     global surprise
+    global gsurprise
 
+    # Die Label werden bei Knopfdruck, aber vorm Erstellen des neuen Labels, gelöscht
     auswahl.destroy()
     error.destroy()
     bezahl.destroy()
     surprise.destroy()
+    gsurprise.destroy()
 
+    # found als Hilfsvariable, um Existenz eines Listeneintrags zu bestätigen
     found = False
+    # Erstellung einer Zufallsauswahl für die Eingabemöglichkeit "Überraschung"
     rando = random.choice(drinks)
 
+    # Eingabe wird mit Drink-Liste abgeglichen
+    # Wenn Übereinstimmung gefunden, dann wird die Bestellung bestätigt
     for x in drinks:
         if choiceSet.lower() == x.lower() or choiceSet.lower() == "Überraschung".lower():
             auswahl_text = f"\nDu hast {numberSet}x {choiceSet.capitalize()} gewählt."
@@ -100,50 +103,60 @@ def submit():
             found = True
             break
 
+    # Wenn nicht gefunden, gib eine Fehlermeldung aus
     if not found:
         error_text = f"\n{choiceSet.capitalize()} ist keine gültige Eingabe. Bitte versuche es noch einmal."
         error = tk.Label(text=error_text, font=mainFont)
         error.grid()
         return
 
-    # Nutzer gibt die Anzahl des Getränkes ein, das er möchte.
-    # Die Schleife stopt in beiden Listen (drinks, prices), sobald der erste Treffer gelandet ist.
-    # Dannach wird die angegebene Menge mit dem Listenpreis multipliziert und ausgegeben.
-
+    # die angegebene Anzahl wird sicherheitshalber als int definiert
     numberSet = int(numberSet)
+
+    # Beide Listen (Drinks, Preise) werden durchsucht
+    # In beiden Listen wird gestoppt, sobald der erste Treffer gefunden wurde
+    # Die bestellte Menge wird mit dem Listenpreis multipliziert
+    # Bestellung mit Gesamtpreis wird ausgegeben
     for x, y in zip(drinks, prices):
         if choiceSet.lower() == x.lower():
             bezahl_text = f"""\n{numberSet} {choiceSet.capitalize()} kosten insgesamt {numberSet*y:.2f} €.\n\nMöchtest du bar zahlen oder mit Karte?"""
             bezahl = tk.Label(text=bezahl_text, font=mainFont)
             bezahl.grid()
             break
-
+        # Das Gleiche, wenn die Eingabe "Überraschung" war
         elif choiceSet.lower() == "Überraschung".lower():
-            for a, b in zip(drinks, prices):
-                if rando == a:
-                    surprise_text = f"""Es wurde {rando.capitalize()} für dich ausgewählt. {numberSet} {rando.capitalize()} kosten insgesamt {numberSet*b:.2f} €.\n\nMöchtest du bar zahlen oder mit Karte?"""
-                    surprise = tk.Label(text=surprise_text, font=mainFont)
-                    surprise.grid()
-                    break
+            if rando == x and rando != "Gummibärchen":
+                surprise_text = f"""Es wurde {rando.capitalize()} für dich ausgewählt. {numberSet} {rando.capitalize()} kosten insgesamt {numberSet*y:.2f} €.\n\nMöchtest du bar zahlen oder mit Karte?"""
+                surprise = tk.Label(text=surprise_text, font=mainFont)
+                surprise.grid()
 
-                elif rando == "Gummibärchen".lower():
-                    surprise_text = f"""Überraschung! Du bekommst eine Tüte {rando.capitalize()}. Das kostet dich nichts! Viel Spaß damit!"""
-                    surprise = tk.Label(text=surprise_text, font=mainFont)
-                    surprise.grid()
-                    break
+            # Die Wahl "Überraschung" hat auch die Chance, gratis Gummibärchen auszuspucken
+            elif rando == "Gummibärchen":
+                gsurprise_text = f"""Überraschung! Du bekommst eine Tüte {rando.capitalize()}. Das kostet dich nichts! Viel Spaß damit!"""
+                gsurprise = tk.Label(text=gsurprise_text, font=mainFont)
+                gsurprise.grid()
+                break
 
 
-numberSet = varNumber.get()
-choiceSet = varChoice.get()
 choice = tk.Label(text="Deine Wahl: ", font=mainFont)
 choice.grid()
 
-# Vorschautext in Eingabefeldern werden definiert und an das Reinklicken ins Feld gebunden
 
-choiceEntry = (tk.Entry(textvariable=varChoice))
+# def divide_drinks():
+#     for item in drinks:
+#         yield item
+#
+
+#for value in divide_drinks():
+    
+
+# Vorschautext in Eingabefeldern werden definiert und an das Reinklicken ins Feld gebunden
+choiceEntry = ttk.Combobox(values=["Kaffee", "Tee", "Wasser", "Kakao", "Überraschung"])
 choiceEntry.grid()
-choiceEntry.insert(0, "Getränk")
-choiceEntry.configure(state="disabled")
+choiceEntry.insert(0, " ")
+#choiceEntry.configure(state="disabled")
+
+button = ttk.Button(text="Auswahl", command=submit)
 
 numberEntry = tk.Entry(textvariable=varNumber)
 numberEntry.grid()
@@ -162,5 +175,7 @@ number_focus_out = numberEntry.bind(
 
 choiceButton = tk.Button(text="Bestellen", command=submit)
 choiceButton.grid()
+
+# das TKinter Fenster mit allen oben festgelegten Inhalten wird initialisiert
 
 window.mainloop()
